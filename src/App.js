5dc8f5dc920b89ad0dde3241aa8469d742604040
AngelicaTheran <atheran082@gmail.com>
2021-06-25 11:03:37 -0500

    Implementacion de rutas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router, Route, Switch, Link, userRouteMatch, userParmas } from "react-router-dom";
+import inicio from './componentes/Inicio';
+import mesero from './componentes/Mesero';
+import cocina from './componentes/Cocina';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>   
+      <Link to="/mesero">
+        Mesero
+      </Link>
+      <Link to="/cocina">
+        Cocina
+      </Link>
+      <Switch>
+      <Route exact path="/" component={inicio}/>
+              
+        <Route path="/mesero" component={mesero}/>
+        
+        <Route path="/cocina" component={cocina}/>        
+      </Switch>
+    
+    </Router>
   );
 }
 
